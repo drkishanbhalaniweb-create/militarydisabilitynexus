2f4080acccdb9247e019233708077d5cd1c76ddc
chetan21122004 <chetan44rt@gmail.com>
2026-01-10 15:33:44 +0530

    .

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // Ensure "REACT_APP_" vars are available if you are lazily migrating them mentally, 
-    // but Next only exposes "NEXT_PUBLIC_" to the client.
     // We will strictly migrate to NEXT_PUBLIC_, so we don't need env mappings here unless strictly necessary.
-    distDir: 'build',
 
     images: {
         remotePatterns: [
